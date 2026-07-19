0f206527d96384f09bcaab358f4886e94819442d
luis.boden <luis.boden@gmail.com>
2026-07-19 12:45:36 +0200

    first draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,2 @@
-pub mod buttons;
-pub mod error;
-pub mod events;
 pub mod wiiuse;
-
-pub use buttons::*;
-pub use events::*;
-
-use std::ffi::CStr;
-use wiiuse_sys::*;
-
-pub fn get_version() -> String {
-    unsafe {
-        let c_str_ptr = wiiuse_version();
-        if c_str_ptr.is_null() {
-            return "unknown".to_string();
-        }
-        let c_str = CStr::from_ptr(c_str_ptr);
-        let str = c_str.to_str().map(|s| s.to_owned());
-        match str {
-            Ok(s) => s,
-            Err(_) => "utf8-error".to_string(),
-        }
-    }
-}
+pub use wiiuse::*;
--- src/wiiuse.rs
+++ src/wiiuse.rs
@@ -1,6 +1,5 @@
-use std::{default, ffi::CStr};
-
-use crate::{error::Error, get_version};
+use bitflags::bitflags;
+use std::ffi::CStr;
 
 pub type WiimotePtrArr = *mut *mut wiiuse_sys::wiimote_t;
 pub type WiimotePtr = *mut wiiuse_sys::wiimote_t;
@@ -196,6 +195,31 @@ impl<'a> Wiimote<'a> {
 
     pub fn get_battery_level(&self) -> f32 {
         unsafe { (*self.ptr).battery_level }
+    }
+}
+
+bitflags! {
+    pub struct WiimoteButton: u16 {
+        const LEFT      = 0x0001;
+        const RIGHT     = 0x0002;
+        const DOWN      = 0x0004;
+        const UP        = 0x0008;
+        const PLUS      = 0x0010;
+        const TWO       = 0x0100;
+        const ONE       = 0x0200;
+        const B         = 0x0400;
+        const A         = 0x0800;
+        const MINUS     = 0x1000;
+        const HOME      = 0x8000;
+    }
+}
+
+impl<'a> Wiimote<'a> {
+    pub fn is_button_pressed(&self, buttons: WiimoteButton) -> bool {
+        unsafe {
+            let current_btns = (*self.ptr).btns;
+            (current_btns & buttons.bits()) != 0
+        }
     }
 }
 
